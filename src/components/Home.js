f88a2a3354b7c409af24e74d66ac685402a5d7ff
rin1782 <colaerin@yahoo.com>
2021-07-01 06:40:27 -0400

    setup started

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+
+
+function Home () {
+    return (
+        <div>
+            <h1 className="header">At Your Door</h1>
+            <p className="description-home">An on-the-go event planning service</p>
+        </div>
+    )
+}
+
+export default Home;
